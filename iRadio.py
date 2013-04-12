f79758c5413b8fb27fd8b144cb5760ebe80d208d
Alexander Danmayer <AppDeveloper@innologix.at>
2013-04-12 20:06:42 +0200

    classify

--- iRadio.py
+++ iRadio.py
@@ -5,158 +5,166 @@ from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 import os
 import re
 
-MAXCHARS = 16
+class IRadio():
+  MAXCHARS = 16
+  BTNS = (lcd.LEFT,lcd.UP,lcd.DOWN,lcd.RIGHT,lcd.SELECT)
+  # Initialize the LCD plate.  Should auto-detect correct I2C bus.  If not,
+  # pass '0' for early 256 MB Model B boards or '1' for all later versions
+  lcd          = Adafruit_CharLCDPlate(0)
+  isplaying    = False
+  isdisplaying = TRUE
+  lastMessage  = ""
 
-def centerLine(msg, fillChar = ' '):
-  if len(msg) < MAXCHARS - 1:
-    return fillChar*(int((MAXCHARS - len(msg))/2)) + msg + fillChar*(MAXCHARS - len(msg) - int((MAXCHARS - len(msg))/2))
-  else: 
-    return msg
+  def centerLine(self, msg, fillChar = ' '):
+    if len(msg) < self.MAXCHARS - 1:
+      return fillChar*(int((self.MAXCHARS - len(msg))/2)) + msg + fillChar*(self.MAXCHARS - len(msg) - int((self.MAXCHARS - len(msg))/2))
+    else: 
+      return msg
 
-def centerHighlight(msg):
-  if len(msg) < (MAXCHARS - 4):
-    newMsg = '< ' + msg + ' >'
-    return centerLine(newMsg, '-')
-  else:
-    return msg
+  def centerHighlight(self, msg):
+    if len(msg) < (self.MAXCHARS - 4):
+      newMsg = '< ' + msg + ' >'
+      return centerLine(newMsg, '-')
+    else:
+      return msg
 
-def clear():
-  global lcd
-  global lastMessage
-  lcd.clear()
-  lastMessage = ""
-  sleep (0.01)
+  def clear(self):
+    self.lcd.clear()
+    self.lastMessage = ""
+    sleep (0.01)
 
-def moveOutLeft():
-  """ Animation, move text out to the left """
-  for dx in range(1,MAXCHARS):
-    dt = max(dx * dx * 2, 6)
-    lcd.scrollDisplayLeft()
-    sleep(max(1.0/dt, 0.01))
-  clear()
+  def moveOutLeft(self):
+    """ Animation, move text out to the left """
+    for dx in range(1,MAXCHARS):
+      dt = max(dx * dx * 2, 6)
+      self.lcd.scrollDisplayLeft()
+      sleep(max(1.0/dt, 0.01))
+    self.clear()
 
-def moveOutRight():
-  """ Animation, move text out to the right """
-  for dx in range(1,MAXCHARS):
-    dt = max(dx * dx * 2, 6)
-    lcd.scrollDisplayRight()
-    sleep(max(1.0/dt, 0.01))
-  clear()
+  def moveOutRight(self):
+    """ Animation, move text out to the right """
+    for dx in range(1,MAXCHARS):
+      dt = max(dx * dx * 2, 6)
+      self.lcd.scrollDisplayRight()
+      sleep(max(1.0/dt, 0.01))
+    self.clear()
 
-def getRadioInfo():
-  """ Get station name and song title from radio stream """
-  global isplaying
-  txt = os.popen("mpc current")
-  string = ""
-  station = ""
-  song    = ""
-  for line in txt.readlines():
-    string += line
-  
-  matchObj = re.search (r'\s*(.*)\s*:\s*(.*)', txt, re.M|re.I)
+  def getRadioInfo(self):
+    """ Get station name and song title from radio stream """
+    txt = os.popen("mpc current")
+    string = ""
+    station = ""
+    song    = ""
+    for line in txt.readlines():
+      string += line
+    
+    matchObj = re.search (r'\s*(.*)\s*:\s*(.*)', string, re.M|re.I)
 
-  if matchObj:
-    station = matchObj.group(1)
-    song    = matchObj.group(2)
-    isplaying = True
-    return [station, song]
-  else:
-    return ["", ""]
+    if matchObj:
+      station = matchObj.group(1)
+      song    = matchObj.group(2)
+      self.isplaying = True
+      return [station, song]
+    else:
+      return ["", ""]
 
-def getWlanIP():
-  """ get network IP from wlan0 """
-  txt = os.popen("ifconfig wlan0 | grep 'inet addr' | awk -F: '{print $2}' | awk '{print $1}'")
-  string = ""
-  for line in txt.readlines():
-    string += line
-  return string
+  def getWlanIP(self):
+    """ get network IP from wlan0 """
+    txt = os.popen("ifconfig wlan0 | grep 'inet addr' | awk -F: '{print $2}' | awk '{print $1}'")
+    string = ""
+    for line in txt.readlines():
+      string += line
+    return string
 
-def bufferedMessage(msg):
-  global lcd
-  global lastMessage
-  if lastMessage != msg:
-    clear()
-    lcd.message(msg)
-    lastMessage = msg
-    sleep(0.01)
-  
-# Initialize the LCD plate.  Should auto-detect correct I2C bus.  If not,
-# pass '0' for early 256 MB Model B boards or '1' for all later versions
-lcd = Adafruit_CharLCDPlate(0)
-isplaying   = False
-lastplaying = True
-lastMessage = ""
-lastInfo    = ["-", "-"]
-counter     = 0
+  def bufferedMessage(self, msg):
+    if self.lastMessage != msg:
+      self.clear()
+      self.lcd.message(msg)
+      self.lastMessage = msg
+      sleep(0.01)
+
+  def toggleDisplay(self):
+    if self.isdisplaying:
+      self.lcd.noDisplay()
+    else:
+      self.lcd.display()
+    self.isdisplaying = not self.isdisplaying
+   
+  def toggleStopPlay(self):
+    if self.isplaying:
+      os.system("mpc stop")
+    else:
+      os.system("mpc play")
+    self.isplaying = not self.isplaying
+
+iRadio = IRadio()
+iRadio.bufferedMessage(iRadio.centerLine("iRadio by Dave") + '\n' + iRadio.centerLine("Have a nice Day"))
+sleep(1)
+iRadio.moveOutLeft()
+
 
 # Poll buttons, display message & set backlight accordingly
-btn = (lcd.LEFT,lcd.UP,lcd.DOWN,lcd.RIGHT,lcd.SELECT)
-pressed = -1
-bufferedMessage(centerLine("iRadio by Dave") + '\n' + centerLine("Have a nice Day"))
-sleep(1)
-moveOutLeft()
+pressed      = -1
+counter      = 0
+lastplaying  = True
+lastInfo     = ["-", "-"]
 
 while True:
   sleep(0.05)
   counter += 1
-  if isplaying and (counter % 100 == 0):
+  if iRadio.isplaying and (counter % 200 == 0):
     lastInfo = ["-", "-"]
 
-  if pressed == -1:
+  if iRadio.pressed == -1:
     prev = -1
   pressed = -1
-  for b in btn:
-    if lcd.buttonPressed(b):
+  for b in iRadio.BTNS:
+    if iRadio.lcd.buttonPressed(b):
       pressed = b
   if pressed is not prev:
     prev = pressed
     if pressed == lcd.SELECT:
-      bufferedMessage(centerLine('my IP is') + '\n' + centerLine(getWlanIP()))
+      iRadio.bufferedMessage(iRadio.centerLine('my IP is') + '\n' + iRadio.centerLine(getWlanIP()))
       sleep(2)
-    if pressed == lcd.LEFT:
-      if isplaying:
-        os.system("mpc stop")
-        isplaying = False
-      else:
-        os.system("mpc play")
-        isplaying = True
+    if pressed == iRadio.lcd.LEFT:
+      iRadio.toggleStopPlay()
       lastInfo = ['-','-']
-    elif pressed == lcd.UP:
+    elif pressed == iRadio.lcd.UP:
       os.system("mpc next")
       sleep(0.3)
       os.system("mpc play")
-      moveOutRight()
+      iRadio.moveOutRight()
       lastInfo = ['-','-']
-    elif pressed == lcd.DOWN:
+    elif pressed == iRadio.lcd.DOWN:
       os.system("mpc prev")
       sleep(0.3)
       os.system("mpc play")
-      moveOutLeft()
+      iRadio.moveOutLeft()
       lastInfo = ['-','-']
-    elif pressed == lcd.RIGHT:
+    elif pressed == iRadio.lcd.RIGHT:
       # bufferedMessage("Play")
       os.system("mpc play")
       lastInfo = ['-','-']
   else:
     stationInfo = getRadioInfo()
-    if (stationInfo[0] != lastInfo[0]) or (stationInfo[1] != lastInfo[1]) or (lastplaying != isplaying):
-      if not isplaying or stationInfo[0] == "":
-        lastMessage = "-"
-        clear()
-        lcd.message(centerLine('iRadio') + "\n" + centerHighlight('PAUSED'))
+    if (stationInfo[0] != lastInfo[0]) or (stationInfo[1] != lastInfo[1]) or (lastplaying != iRadio.isplaying):
+      if not iRadio.isplaying or stationInfo[0] == "":
+        iRadio.lastMessage = "-"
+        iRadio.clear()
+        iRadio.lcd.message(iRadio.centerLine('iRadio') + "\n" + iRadio.centerHighlight('PAUSED'))
       elif stationInfo[0] != lastInfo[0]:
         # station change 
         if len(stationInfo[0]) < 16:
-          bufferedMessage(centerLine('iRadio') + '\n' +  centerHighlight(stationInfo[0][:16].strip()))
+          iRadio.bufferedMessage(iRadio.centerLine('iRadio') + '\n' +  iRadio.centerHighlight(stationInfo[0][:16].strip()))
         else:
           message = stationInfo[1][:16].lstrip() + '\n' + stationInfo[1][16:32].lstrip()
-          bufferedMessage(message)
+          iRadio.bufferedMessage(message)
         stationInfo[1] = ""
         sleep(1.5)
       elif stationInfo[1] != lastInfo[1]:
         # song changed
         message = stationInfo[1][:16] + '\n' + stationInfo[1][16:32]
-        bufferedMessage(message)
+        iRadio.bufferedMessage(message)
       #sleep
       lastInfo = stationInfo
       lastplaying = isplaying
